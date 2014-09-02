506e244381a1b1ae6e7009d5e9f575fed6bcae7f
Guy Halperin & Sergii Mertsalov <student@epicodus.com>
2014-09-02 11:06:54 -0700

    add questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,30 @@
+class QuestionsController < ApplicationController
+   before_filter :authorize, only: [:edit, :update]
+   def index
+    @questions = Question.all
+   end
+   def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+    if @question.save
+      redirect_to root_url, notice: "A new question added!"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+private
+  def question_params
+     params.require(:question).permit(:title, :description, :user_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'questions#index'
   get 'sessions/new'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+
   resources :users
   resources :sessions
+  resources :questions
 end
