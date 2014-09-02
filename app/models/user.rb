1ce143e0c8f1f42ab521618f7e816e06dc0bf8b1
Guy Halperin & Sergii Mertsalov <student@epicodus.com>
2014-09-02 10:23:10 -0700

    add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :user_name
+
+  validates :email, presence: true,
+                  uniqueness: { case_sensitive: false },
+                  format: {
+                    with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/
+                  }
+
+  before_save :downcase_userinfo
+
+private
+  def downcase_userinfo
+    self.user_name = self.user_name.downcase if self.user_name
+    self.email = self.email.downcase if self.email
+  end
+
 end
